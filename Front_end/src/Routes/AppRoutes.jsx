19e64a78c3eb1fc1e3e48f339a87a1aa72cf4217
Lucas lopes <lucassantos124578@gmail.com>
2026-09-14 23:12:20 -0300

    corrige casing das pastas no approutes

--- Front_end/src/Routes/AppRoutes.jsx
+++ Front_end/src/Routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from '../pages/login/login.jsx'
 import Register from '../pages/Register/Register.jsx'
-import Dashboard from '../pages/painel_adm/Dashboard.jsx'
+import Dashboard from '../pages/Painel_adm/Dashboard.jsx'
 
 function AppRoutes() {
   return (
