db075ec1bacda98193c3e064fcfb857fc3d835fc
Lucas lopes <lucassantos124578@gmail.com>
2026-09-15 16:30:15 -0300

    Conecta frontend ao backend Render

--- Front_end/src/pages/Painel_adm/Dashboard.jsx
+++ Front_end/src/pages/Painel_adm/Dashboard.jsx
@@ -19,7 +19,7 @@ function Dashboard() {
 
     try {
       const response = await fetch(
-        'http://localhost:5000/buscar-preco',
+        'https://back-end-waf6.onrender.com/buscar-preco',
         {
           method: 'POST',
           headers: {
